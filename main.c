1dc6c1d6e1dc709461681977d6570bd9eeefb70d
JackGrylls <jackgrylls87@gmail.com>
2025-12-21 02:09:42 +0000

    We can now read the entire bmp file into an array, without having to know the size.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Offset and size of the "size" header in the BMP file
 #define SZ_OFFSET 2
@@ -6,6 +7,7 @@
 
 int getFilesize(FILE *fptr)
 {
+    rewind(fptr);
     // From https://en.wikipedia.org/wiki/BMP_file_format
     int offset = 2;
     int size = 4;
@@ -17,15 +19,34 @@ int getFilesize(FILE *fptr)
     // The first two bytes in a bmp file are a header field identifying the BMP
     // The next four represent the file size
     // These bitwise operations read those four bytes and convert them into an integer
+    
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     printf("%08b\n",contents[i]);
+    // }
+
     int test = 0;
     test = test^(contents[5] << 24);
     test = test^(contents[4] << 16);
     test = test^(contents[3] << 8);
     test = test^(contents[2]);
 
+    rewind(fptr);
     return test;
 }
 
+__uint8_t * readFile(FILE *fptr)
+{   
+    rewind(fptr);
+
+    int size = getFilesize(fptr);
+    __uint8_t * contents = malloc(sizeof(__uint8_t)*size);
+    fgets(contents,size,fptr);
+    
+    
+    
+    return contents;
+}
 
 int main(int argc, char**argv)
 {
@@ -34,12 +55,13 @@ int main(int argc, char**argv)
         printf("Usage: ./main.h <filename>.bmp\n");
         return 0;
     }
+    
+    FILE *fptr = fopen(argv[1],"r");
 
-    char* filename = argv[1];
-    FILE *fptr;
-    fptr = fopen(filename,"r");
-    // 39,494 bytes
+    __uint8_t * file = readFile(fptr);
 
-    int filesize = getFilesize(fptr);
-    printf("Size: %d\n",filesize);
+    for(int i = 0; i < 128; i++)
+    {
+        printf("%d: %0x\n",i,file[i]);
+    }
 }
